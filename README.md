f659f811fe43d99e98e275135a6ce53f3c00cc9a
Shelley McHardy <shelley.mchardy@gmail.com>
2021-06-02 14:57:28 -0400

    added readme and updated to do list

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Weather-Dashboard
+
+This app allows the user to enter a city name and retrieve current weather as well as a 5-day forecast. It saves previous city searches and makes them available at the touch of a button.
+
+Unfortunately, it still needs some work. The user can enter a city name and return the current city name, date, temperature, windspeed, and humidity. The weather icon and UV index are not yet included. The 5-day forecast and history are also not yet functional.
+
+
+![screenshot of app](weather.png)
+
+
+Deployed application:
+https://shelleymcq.github.io/Weather-Dashboard/
+
+
+
+Built with:
+    HTML
+    CSS
+    Bulma CSS framework
+    JavaScript
+    JQuery
+    APIs:
+    openweathermap.org
+    positionstack.com
+    momentjs.com
--- script.js
+++ script.js
@@ -11,117 +11,122 @@ const getCurrentHumidity = document.getElementById("current-humidity");
 const getCurrentUV = document.getElementById("uv-index");
 
 
+
+
+
+// using 5-day forecast until I can fix one call, so missing UV index
 $("#search-button").click(function () {
   let cityEntered = $(this).siblings(".input").val();
   localStorage.setItem("city", cityEntered)
-  getLatLong(cityEntered)
+  getCurrentWeather(cityEntered)
 })
 
-
-function getLatLong(cityInput) {
-  // This api requires the city name to start with uppercase. And it's REALLY slow.
-  fetch(`http://api.positionstack.com/v1/forward?access_key=826f1ac393c4931d61713771643eac12&query=${cityInput}&limit=1&output=json`)
+function getCurrentWeather(city) {
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=3b143d5aec8fa14dd06be97585fc0e4c`)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-        let lati = (data.data[0].latitude)
-        let longi = (data.data[0].longitude)
-        localStorage.setItem("lat", lati);
-        localStorage.setItem("long", longi);
-        getCurrentWeather(lati, longi);
+        getCurrentHeader.textContent = (data.city.name + " (" + data.list[0].dt_txt + ") " + data.list[0].weather[0].icon);
+        getCurrentTemp.innerText += (data.list[0].main.temp);
+        getCurrentWind.innerText += (data.list[0].wind.speed);
+        getCurrentHumidity.innerText += (data.list[0].main.humidity);
     });
   }
 
-function getCurrentWeather(lat, lon) {
-  fetch("https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=3b143d5aec8fa14dd06be97585fc0e4c")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data)
-  });
 
-}
+// get data from localStorage to use in search history
+let latitude = JSON.parse(localStorage.getItem('lat'));
+console.log(latitude)
+let longitude = JSON.parse(localStorage.getItem('long'));
+console.log(longitude)
+let city = localStorage.getItem('city');
+console.log(city)
 
 
 
-// // get data from localStorage
-//   let latitude = JSON.parse(localStorage.getItem('lat'));
-//   console.log(latitude)
-//   let longitude = JSON.parse(localStorage.getItem('long'));
-//   console.log(longitude)
-//   let city = localStorage.getItem('city');
-//   console.log(city)
 
 
 
-    
-  // fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.7698&lon=-84.4145&appid=3b143d5aec8fa14dd06be97585fc0e4c")
-  // .then(function (response) {
-  //   return response.json();
-  // })
-  // .then(function (data) {
-  //       console.log(data)
-  // });
+// To Do Still
 
+// fix date
+// get the weather icons linked
 
-// using 5-day forecast to practice before using one call
-// $("#search-button").click(function () {
-//   let cityEntered = $(this).siblings(".input").val();
-//   localStorage.setItem("city", cityEntered)
-//   getCurrentWeather(cityEntered)
-// })
+// // need to sort out the 5 days from the every 3-hour data returned. The first 5 elements are just a day and a half at 3 hour intervals
 
-// function getCurrentWeather(city) {
-//   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=3b143d5aec8fa14dd06be97585fc0e4c`)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//         getCurrentHeader.textContent = (data.city.name + " (" + data.list[0].dt_txt + ") " + data.list[0].weather[0].icon);
-//         getCurrentTemp.innerText += (data.list[0].main.temp);
-//         getCurrentWind.innerText += (data.list[0].wind.speed);
-//         getCurrentHumidity.innerText += (data.list[0].main.humidity);
-//     });
-//   }
-
-  
-// input city added to local storage on button click
-// $("#search-button").click(function () {
-//   let cityEntered = $(this).siblings(".input").val();
-//   localStorage.setItem("city", cityEntered)
-//   getCityForecast(cityEntered)
-// })
-
-// fetches 5+ days
+// // fetches 5+ days
 // function getCityForecast(city) {
 //   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=3b143d5aec8fa14dd06be97585fc0e4c`)
 //     .then(function (response) {
 //       return response.json();
 //     })
 //     .then(function (data) {
-//       for (let i = 0; i < 5; i++) {
-//         console.log(data.list[i].dt_txt);
-//         console.log(data.list[i].weather[0].icon);
-//         console.log(data.list[i].main.temp);
-//         console.log(data.list[i].wind.speed);
-//         console.log(data.list[i].main.humidity);
-
-//       }
+//      for (let i = 0; i < 5; i++) {
+//        console.log(data.list[i].dt_txt);
+//        console.log(data.list[i].weather[0].icon);
+//        console.log(data.list[i].main.temp);
+//        console.log(data.list[i].wind.speed);
+//        console.log(data.list[i].main.humidity);
 //     });
+//   }
 
+
+// Successfully gets lat/long, but I can't get that into one call yet.
+
+// $("#search-button").click(function () {
+//   let cityEntered = $(this).siblings(".input").val();
+//   localStorage.setItem("city", cityEntered)
+//   getLatLong(cityEntered)
+// })
+
+
+// function getLatLong(cityInput) {
+//   // This api requires the city name to start with uppercase. And it's REALLY slow.
+//   fetch(`http://api.positionstack.com/v1/forward?access_key=826f1ac393c4931d61713771643eac12&query=${cityInput}&limit=1&output=json`)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//         let lati = (data.data[0].latitude)
+//         let longi = (data.data[0].longitude)
+//         localStorage.setItem("lat", lati);
+//         localStorage.setItem("long", longi);
+//         getCurrentWeather(lati, longi);
+//     });
+//   }
+
+//   // Need this in jQuery land so the variables work??
+// function getCurrentWeather(lat, lon) {
+//   fetch("https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=3b143d5aec8fa14dd06be97585fc0e4c")
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data)
+//   });
 // }
 
 
+// // input city added to local storage on button click
+// $("#search-button").click(function () {
+//   let cityEntered = $(this).siblings(".input").val();
+//   localStorage.setItem("city", cityEntered)
+//   getCityForecast(cityEntered)
+//   //addCity(cityEntered)
+// })
+
+// // store previously searched cities in an array (not working yet)
+// let addCity = function(city) {
+//   let cityArray = []
+//   cityArray.push(city);
+//   console.log(cityArray);
+// }
+
+// stored cities on screen
 
 
 
-// Still Need:
-// 1. 
-// 2. feed it to one call 
-// 3. rework getCurrentWeather
-// 4. print 5-day to 5 boxes !(*&^$(*&%))
-// 5. store cities 
-// 6. print 5-6 saved cities to previous buttons 
-// 7. icons!!
+
+
+
