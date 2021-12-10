38efc8ed4e53482d52b3cc6662059a7ed9e21646
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-12-10 16:11:55 -0500

    fixed clear and delay on new search

--- script.js
+++ script.js
@@ -11,12 +11,26 @@ const getCurrentWind = document.getElementById("current-wind");
 const getCurrentHumidity = document.getElementById("current-humi");
 const getCurrentUV = document.getElementById("current-uvin");
 
-$("#search-button").click(function () {
-  let cityEntered = $(this).siblings(".input").val();
-  localStorage.setItem("city", cityEntered)
-  getLatLon(cityEntered)
-  getCurrent(cityEntered)
-})
+function getCurrent(city, lat, lon) {
+
+  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${owAPIKey}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+
+      let formatDate = moment.unix(data.current.dt).format("MMMM D, YYYY");
+
+      getCurrentHeader.textContent = (city);
+      getCurrentIcon.textContent = (data.current.weather[0].icon);
+      getCurrentDate.textContent = (formatDate);
+      getCurrentTemp.textContent = ("  " + data.current.temp + " \xB0F");
+      getCurrentWind.textContent = ("  " + data.current.wind_speed + " mph");
+      getCurrentHumidity.textContent = ("  " + data.current.humidity + "%");
+      getCurrentUV.textContent = ("  " + data.current.uvi);
+    })
+}
 
 function getLatLon(city) {
   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${owAPIKey}`)
@@ -24,38 +38,20 @@ function getLatLon(city) {
       return response.json();
     })
     .then(function (data) {
-        localStorage.setItem("lat", data.city.coord.lat);
-        localStorage.setItem("lon", data.city.coord.lon);
+        let lat = data.city.coord.lat;
+        let lon = data.city.coord.lon;
+
+        getCurrent(city, lat, lon);
     });
   }
   
-  function getCurrent(city) {
-    let lat = localStorage.getItem("lat");
-    let lon = localStorage.getItem("lon");
-
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${owAPIKey}`)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-
-        let formatDate = moment.unix(data.current.dt).format("MMMM D, YYYY");
-
-        getCurrentHeader.textContent = (city);
-        getCurrentIcon.textContent = (data.current.weather[0].icon);
-        getCurrentDate.textContent = (formatDate);
-        getCurrentTemp.innerText += (" " + data.current.temp + " ");
-        getCurrentWind.innerText += (" " + data.current.wind_speed + " ");
-        getCurrentHumidity.innerText += (" " + data.current.humidity + " ");
-        getCurrentUV.innerText += (" " + data.current.uvi + " ");
-      })
-  }
+$("#search-button").click(function () {
+  let cityEntered = $(this).siblings(".input").val();
+  localStorage.setItem("city", cityEntered)
+  getLatLon(cityEntered)
+})
 
 
-
-
-  // clear current on new search
   // render 5 day forecast to cards
   // render icons
 
