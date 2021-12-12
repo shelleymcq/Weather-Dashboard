42b7b106d27420e5228484a5b72b1b2287e8b4c7
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-12-12 15:19:55 -0500

    rendering 5-day forecast

--- script.js
+++ script.js
@@ -11,9 +11,37 @@ const getCurrentWind = document.getElementById("current-wind");
 const getCurrentHumidity = document.getElementById("current-humi");
 const getCurrentUV = document.getElementById("current-uvin");
 
-const getForecastOne = document.getElementById("one");
+const getDateOne = document.getElementById("day1-date");
+const getIconOne = document.getElementById("day1-icon");
+const getTempOne = document.getElementById("day1-temp");
+const getWindOne = document.getElementById("day1-wind");
+const getHumiOne = document.getElementById("day1-humi");
 
-function getCurrent(city, lat, lon) {
+const getDateTwo = document.getElementById("day2-date");
+const getIconTwo = document.getElementById("day2-icon");
+const getTempTwo = document.getElementById("day2-temp");
+const getWindTwo = document.getElementById("day2-wind");
+const getHumiTwo = document.getElementById("day2-humi");
+
+const getDateThree = document.getElementById("day3-date");
+const getIconThree = document.getElementById("day3-icon");
+const getTempThree = document.getElementById("day3-temp");
+const getWindThree = document.getElementById("day3-wind");
+const getHumiThree = document.getElementById("day3-humi");
+
+const getDateFour = document.getElementById("day4-date");
+const getIconFour = document.getElementById("day4-icon");
+const getTempFour = document.getElementById("day4-temp");
+const getWindFour = document.getElementById("day4-wind");
+const getHumiFour = document.getElementById("day4-humi");
+
+const getDateFive = document.getElementById("day5-date");
+const getIconFive = document.getElementById("day5-icon");
+const getTempFive = document.getElementById("day5-temp");
+const getWindFive = document.getElementById("day5-wind");
+const getHumiFive = document.getElementById("day5-humi");
+
+function getWeather(city, lat, lon) {
 
   fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${owAPIKey}`)
     .then(function (response) {
@@ -22,20 +50,63 @@ function getCurrent(city, lat, lon) {
     .then(function (data) {
       console.log(data)
 
+      // render current weather
       let formatDate = moment.unix(data.current.dt).format("MMMM D, YYYY");
-      let icon = data.current.weather[0].icon;
+      let iconCurrent = data.current.weather[0].icon;
 
       getCurrentHeader.textContent = (city.charAt(0).toUpperCase() + city.slice(1));
-      http://openweathermap.org/img/w/10d.png
-      getCurrentIcon.innerHTML = (`<img src="http://openweathermap.org/img/w/${icon}.png">`);
+      getCurrentIcon.innerHTML = (`<img src="http://openweathermap.org/img/w/${iconCurrent}.png">`);
       getCurrentDate.textContent = (formatDate);
       getCurrentTemp.textContent = ("  " + data.current.temp + " \xB0F");
       getCurrentWind.textContent = ("  " + data.current.wind_speed + " mph");
       getCurrentHumidity.textContent = ("  " + data.current.humidity + "%");
       getCurrentUV.textContent = ("  " + data.current.uvi);
 
-      // this is very wrong????keep trying
-      getForecastOne.child(".date").textContent = (data.daily[1].dt);
+      // render 5-day forecast
+      let formatDateOne = moment.unix(data.daily[1].dt).format("M/D/YY");
+      let iconOne = data.daily[1].weather[0].icon;
+
+      getDateOne.textContent = (formatDateOne);
+      getIconOne.innerHTML = (`<img src="http://openweathermap.org/img/w/${iconOne}.png">`);
+      getTempOne.textContent = ("  " + data.daily[1].temp.max + " \xB0F");
+      getWindOne.textContent = ("  " + data.daily[1].wind_speed + " mph");
+      getHumiOne.textContent = ("  " + data.daily[1].humidity + "%");
+
+      let formatDateTwo = moment.unix(data.daily[2].dt).format("M/D/YY");
+      let iconTwo = data.daily[2].weather[0].icon;
+
+      getDateTwo.textContent = (formatDateTwo);
+      getIconTwo.innerHTML = (`<img src="http://openweathermap.org/img/w/${iconTwo}.png">`);
+      getTempTwo.textContent = ("  " + data.daily[2].temp.max + " \xB0F");
+      getWindTwo.textContent = ("  " + data.daily[2].wind_speed + " mph");
+      getHumiTwo.textContent = ("  " + data.daily[2].humidity + "%");
+
+      let formatDateThree = moment.unix(data.daily[3].dt).format("M/D/YY");
+      let iconThree = data.daily[3].weather[0].icon;
+
+      getDateThree.textContent = (formatDateThree);
+      getIconThree.innerHTML = (`<img src="http://openweathermap.org/img/w/${iconThree}.png">`);
+      getTempThree.textContent = ("  " + data.daily[3].temp.max + " \xB0F");
+      getWindThree.textContent = ("  " + data.daily[3].wind_speed + " mph");
+      getHumiThree.textContent = ("  " + data.daily[3].humidity + "%");
+      
+      let formatDateFour = moment.unix(data.daily[4].dt).format("M/D/YY");
+      let iconFour = data.daily[4].weather[0].icon;
+
+      getDateFour.textContent = (formatDateFour);
+      getIconFour.innerHTML = (`<img src="http://openweathermap.org/img/w/${iconFour}.png">`);
+      getTempFour.textContent = ("  " + data.daily[4].temp.max + " \xB0F");
+      getWindFour.textContent = ("  " + data.daily[4].wind_speed + " mph");
+      getHumiFour.textContent = ("  " + data.daily[4].humidity + "%");
+      
+      let formatDateFive = moment.unix(data.daily[5].dt).format("M/D/YY");
+      let iconFive = data.daily[5].weather[0].icon;
+
+      getDateFive.textContent = (formatDateFive);
+      getIconFive.innerHTML = (`<img src="http://openweathermap.org/img/w/${iconFive}.png">`);
+      getTempFive.textContent = ("  " + data.daily[5].temp.max + " \xB0F");
+      getWindFive.textContent = ("  " + data.daily[5].wind_speed + " mph");
+      getHumiFive.textContent = ("  " + data.daily[5].humidity + "%");
 
     })
 }
@@ -49,7 +120,7 @@ function getLatLon(city) {
         let lat = data.city.coord.lat;
         let lon = data.city.coord.lon;
 
-        getCurrent(city, lat, lon);
+        getWeather(city, lat, lon);
     });
   }
   
@@ -61,7 +132,6 @@ $("#search-button").click(function () {
 
 
   // render 5 day forecast to cards
-  // render icons
 
   // search history
 
